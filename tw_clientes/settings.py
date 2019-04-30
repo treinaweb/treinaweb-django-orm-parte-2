2193c32573ae70ad342282fb16962b26f863fe09
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-04-30 15:35:36 -0300

    03.05_Configurando_Redis_Aplicacao

--- tw_clientes/settings.py
+++ tw_clientes/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        },
+        'KEY_PREFIX': 'django_orm'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
